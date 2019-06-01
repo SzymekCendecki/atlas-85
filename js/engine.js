4cc4c36fb3c731d08c88628fe03162bf4bfc2db0
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2019-06-01 13:36:15 +0200

    Add files via upload

--- js/engine.js
+++ js/engine.js
@@ -84,6 +84,8 @@ document.addEventListener("DOMContentLoaded", function () {
         dataType: 'json'
     }).done(function (data) {
         console.log(data);
+
+        $("#mainContainer").append(data.europaDescription[0].position, data.europaDescription[0].area, data.europaDescription[0].adminDivision, data.europaDescription[0].biggestCities, data.europaDescription[0].population, data.europaDescription[0].naturalConditions, data.europaDescription[0].historyPoliticalOutline, data.europaDescription[0].Economy);
     }).fail(function () {
         console.log("coś nie bangla...");
     });
--- js/workingFile.js
+++ js/workingFile.js
@@ -3,7 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
         url: 'https://szymekcendecki.github.io/atlas-86/json/europa.json',
         type: 'GET',
         dataType: 'json'
-        }).done((data) => {  
-          console.log(data);
-        }).fail(()=>{ console.log("coś nie bangla..."); });  
+    }).done((data) => {  
+        console.log(data);
+
+        $("#mainContainer").append(data.europaDescription[0].position, data.europaDescription[0].area, data.europaDescription[0].adminDivision, data.europaDescription[0].biggestCities, data.europaDescription[0].population, data.europaDescription[0].naturalConditions, data.europaDescription[0].historyPoliticalOutline, data.europaDescription[0].Economy)
+    }).fail(()=>{ 
+        console.log("coś nie bangla..."); 
+    });  
 });
