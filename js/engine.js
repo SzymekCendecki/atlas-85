db37d8660dd0d7002f0e7ef1c1b51e6d589f7258
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2019-10-13 20:27:00 +0200

    Add files via upload

--- js/engine.js
+++ js/engine.js
@@ -77,7 +77,7 @@ module.exports = __webpack_require__(1);
 "use strict";
 
 
-var _functions = __webpack_require__(3);
+var _functions = __webpack_require__(2);
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -89,6 +89,14 @@ document.addEventListener("DOMContentLoaded", function () {
         $("#subListEurope").toggleClass("hide");
         $("#europe").toggleClass("red");
     });
+
+    var x = document.querySelectorAll("#subListEurope > li");
+    console.log(x);
+
+    for (var i = 0; i < x.length; i++) {
+        var y = $(x[i]).attr("id");
+        console.log(y);
+    }
 
     $("#albania").on("click", function () {
         $.ajax({
@@ -202,6 +210,90 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
+    $("#finland").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.finland[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#france").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.france[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#gibraltar").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.gibraltar[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#greece").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.greece[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#spain").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.spain[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#holland").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.holland[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
     $("#irleand").on("click", function () {
         $.ajax({
             url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
@@ -215,11 +307,290 @@ document.addEventListener("DOMContentLoaded", function () {
             console.log("coś nie bangla...");
         });
     });
+
+    $("#iceland").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.iceland[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#yugoslavia").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.yugoslavia[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#liechtenstein").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.liechtenstein[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#luxembourg").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.luxembourg[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#malta").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.malta[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#monaco").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.monaco[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#eastGermany").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.eastGermany[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#norway").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.norway[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#poland").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.poland[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#portugal").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.portugal[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#westGermany").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.westGermany[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#romania").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.romania[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#sanMarino").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.sanMarino[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#switzerland").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.switzerland[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#sweden").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.sweden[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#vatican").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.vatican[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#hungary").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.hungary[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#england").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.england[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#italy").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.italy[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
+
+    $("#cccp").on("click", function () {
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done(function (data) {
+            (0, _functions.showCountry)(data.cccp[0]);
+            $("#subListEurope").toggleClass("hide");
+            $("#mainList").toggleClass("hide");
+        }).fail(function () {
+            console.log("coś nie bangla...");
+        });
+    });
 });
 
 /***/ }),
-/* 2 */,
-/* 3 */
+/* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
--- js/workingFile.js
+++ js/workingFile.js
@@ -11,7 +11,14 @@ document.addEventListener("DOMContentLoaded", () => {
         $("#europe").toggleClass("red");
     });
 
+    let x = document.querySelectorAll("#subListEurope > li");
+    console.log(x);
 
+    for(let i=0; i<x.length; i++){
+        let y = $(x[i]).attr("id");     
+        console.log(y);        
+    }
+    
     $("#albania").on("click", ()=>{
         $.ajax({
             url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
@@ -124,6 +131,90 @@ document.addEventListener("DOMContentLoaded", () => {
         });  
     });    
     
+    $("#finland").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.finland[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });    
+
+    $("#france").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.france[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });    
+
+    $("#gibraltar").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.gibraltar[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });    
+
+    $("#greece").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.greece[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });    
+
+    $("#spain").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.spain[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });    
+
+    $("#holland").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.holland[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });    
+
     $("#irleand").on("click", ()=>{
         $.ajax({
             url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
@@ -137,5 +228,284 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log("coś nie bangla..."); 
         });  
     });
+
+    $("#iceland").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.iceland[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });    
+
+    $("#yugoslavia").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.yugoslavia[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });    
+
+    $("#liechtenstein").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.liechtenstein[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });    
+
+    $("#luxembourg").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.luxembourg[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });   
+    
+    $("#malta").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.malta[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });    
+
+    $("#monaco").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.monaco[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });  
+    
+    $("#eastGermany").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.eastGermany[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    }); 
+    
+    $("#norway").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.norway[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });   
+
+    $("#poland").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.poland[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });   
  
+    $("#portugal").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.portugal[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });   
+
+    $("#westGermany").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.westGermany[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });   
+
+    $("#romania").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.romania[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });   
+
+    $("#sanMarino").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.sanMarino[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });   
+
+    $("#switzerland").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.switzerland[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    }); 
+    
+    $("#sweden").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.sweden[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });   
+
+    $("#vatican").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.vatican[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });   
+
+    $("#hungary").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.hungary[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });   
+
+    $("#england").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.england[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });   
+
+    $("#italy").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.italy[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });  
+    
+    $("#cccp").on("click", ()=>{
+        $.ajax({
+            url: 'https://szymekcendecki.github.io/atlas-85/json/europa.json',
+            type: 'GET',
+            dataType: 'json'
+        }).done((data) => {  
+             showCountry(data.cccp[0]);
+             $("#subListEurope").toggleClass("hide");
+             $("#mainList").toggleClass("hide");
+        }).fail(()=>{ 
+            console.log("coś nie bangla..."); 
+        });  
+    });   
 });
