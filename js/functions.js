911599ee0504b10c0b0525bd129889de4682ffc5
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2019-10-16 18:53:01 +0200

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -3,3 +3,8 @@ export function showCountry(country){
     $("#description").empty();
     $("#description").append(country.title, country.subtitle, country.position, country.area, country.adminDivision, country.capitol, country.biggestCities, country.population, country.naturalConditions, country.language, country.currency, country.historyPoliticalSystem, country.economy, country.map);
 }
+
+export function showHideLists(sublist){
+    $(sublist).toggleClass("hide");
+    $("#mainList").toggleClass("hide");
+}
