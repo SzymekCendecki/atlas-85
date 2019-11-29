e236b68b484e3b88fdaac99dc7038ce38f9d98a8
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2019-11-29 21:27:28 +0100

    Add files via upload

--- js/engine.js
+++ js/engine.js
@@ -79,51 +79,150 @@ module.exports = __webpack_require__(1);
 
 var path = 'https://szymekcendecki.github.io/atlas-85/json/';
 
-var europeArrayJson = [];
-
-var europeArray = ["europeDescription", "albania", "andorra", "austria", "belgium", "westernBerlin", "bulgaria", "czechoslovakia", "denmark", "finland", "france", "gibraltar", "greece", "spain", "holland", "irleand", "iceland", "yugoslavia", "liechtenstein", "luxembourg", "malta", "monaco", "eastGermany", "norway", "poland", "portugal", "westGermany", "romania", "sanMarino", "switzerland", "sweden", "vatican", "hungary", "england", "italy", "cccp"];
+var clickCountry = function clickCountry(country) {
+    document.getElementById("mainContainer").innerHTML = "";
+    document.getElementById("mainContainer").innerHTML = country.title + country.subtitle + country.position + country.area + country.adminDivision + country.capitol + country.biggestCities + country.population + country.naturalConditions + country.language + country.currency + country.historyPoliticalSystem + country.economy + country.map;
+};
 
 fetch(path + 'europa.json').then(function (response) {
     return response.json();
 }).then(function (data) {
-    // Object.keys(data).forEach(element => console.log(element, element.length));  
-    //jsonIteration(europeArrayJson, data);
-    //clickCountry(europeArray, europeArrayJson);
-
     document.getElementById("europeDescription").addEventListener("click", function () {
-        document.getElementById("mainContainer").innerHTML = "";
-        document.getElementById("mainContainer").innerHTML = data.europeDescription[0].title + data.europeDescription[0].subtitle + data.europeDescription[0].position + data.europeDescription[0].area + data.europeDescription[0].adminDivision + data.europeDescription[0].capitol + data.europeDescription[0].biggestCities + data.europeDescription[0].population + data.europeDescription[0].naturalConditions + data.europeDescription[0].language + data.europeDescription[0].currency + data.europeDescription[0].historyPoliticalSystem + data.europeDescription[0].economy + data.europeDescription[0].map;
+        clickCountry(data.europeDescription[0]);
     });
-
     document.getElementById("albania").addEventListener("click", function () {
-        document.getElementById("mainContainer").innerHTML = "";
-        document.getElementById("mainContainer").innerHTML = data.albania[0].title + data.albania[0].subtitle + data.albania[0].position + data.albania[0].area + data.albania[0].adminDivision + data.albania[0].capitol + data.albania[0].biggestCities + data.albania[0].population + data.albania[0].naturalConditions + data.albania[0].language + data.albania[0].currency + data.albania[0].historyPoliticalSystem + data.albania[0].economy + data.albania[0].map;
+        clickCountry(data.albania[0]);
     });
-
     document.getElementById("andorra").addEventListener("click", function () {
-        document.getElementById("mainContainer").innerHTML = "";
-        document.getElementById("mainContainer").innerHTML = data.andorra[0].title + data.andorra[0].subtitle + data.andorra[0].position + data.andorra[0].area + data.andorra[0].adminDivision + data.andorra[0].capitol + data.andorra[0].biggestCities + data.andorra[0].population + data.andorra[0].naturalConditions + data.andorra[0].language + data.andorra[0].currency + data.andorra[0].historyPoliticalSystem + data.andorra[0].economy + data.andorra[0].map;
+        clickCountry(data.andorra[0]);
+    });
+    document.getElementById("austria").addEventListener("click", function () {
+        clickCountry(data.austria[0]);
+    });
+    document.getElementById("belgium").addEventListener("click", function () {
+        clickCountry(data.belgium[0]);
+    });
+    document.getElementById("westernBerlin").addEventListener("click", function () {
+        clickCountry(data.westernBerlin[0]);
+    });
+    document.getElementById("bulgaria").addEventListener("click", function () {
+        clickCountry(data.bulgaria[0]);
+    });
+    document.getElementById("czechoslovakia").addEventListener("click", function () {
+        clickCountry(data.czechoslovakia[0]);
+    });
+    document.getElementById("denmark").addEventListener("click", function () {
+        clickCountry(data.denmark[0]);
+    });
+    document.getElementById("finland").addEventListener("click", function () {
+        clickCountry(data.finland[0]);
+    });
+    document.getElementById("france").addEventListener("click", function () {
+        clickCountry(data.france[0]);
+    });
+    document.getElementById("gibraltar").addEventListener("click", function () {
+        clickCountry(data.gibraltar[0]);
+    });
+    document.getElementById("greece").addEventListener("click", function () {
+        clickCountry(data.greece[0]);
+    });
+    document.getElementById("spain").addEventListener("click", function () {
+        clickCountry(data.spain[0]);
+    });
+    document.getElementById("holland").addEventListener("click", function () {
+        clickCountry(data.holland[0]);
+    });
+    document.getElementById("irleand").addEventListener("click", function () {
+        clickCountry(data.irleand[0]);
+    });
+    document.getElementById("iceland").addEventListener("click", function () {
+        clickCountry(data.iceland[0]);
+    });
+    document.getElementById("yugoslavia").addEventListener("click", function () {
+        clickCountry(data.yugoslavia[0]);
+    });
+    document.getElementById("liechtenstein").addEventListener("click", function () {
+        clickCountry(data.liechtenstein[0]);
+    });
+    document.getElementById("luxembourg").addEventListener("click", function () {
+        clickCountry(data.luxembourg[0]);
+    });
+    document.getElementById("malta").addEventListener("click", function () {
+        clickCountry(data.malta[0]);
+    });
+    document.getElementById("monaco").addEventListener("click", function () {
+        clickCountry(data.monaco[0]);
+    });
+    document.getElementById("eastGermany").addEventListener("click", function () {
+        clickCountry(data.eastGermany[0]);
+    });
+    document.getElementById("norway").addEventListener("click", function () {
+        clickCountry(data.norway[0]);
+    });
+    document.getElementById("poland").addEventListener("click", function () {
+        clickCountry(data.poland[0]);
+    });
+    document.getElementById("portugal").addEventListener("click", function () {
+        clickCountry(data.portugal[0]);
+    });
+    document.getElementById("westGermany").addEventListener("click", function () {
+        clickCountry(data.westGermany[0]);
+    });
+    document.getElementById("romania").addEventListener("click", function () {
+        clickCountry(data.romania[0]);
+    });
+    document.getElementById("sanMarino").addEventListener("click", function () {
+        clickCountry(data.sanMarino[0]);
+    });
+    document.getElementById("switzerland").addEventListener("click", function () {
+        clickCountry(data.switzerland[0]);
+    });
+    document.getElementById("sweden").addEventListener("click", function () {
+        clickCountry(data.sweden[0]);
+    });
+    document.getElementById("vatican").addEventListener("click", function () {
+        clickCountry(data.vatican[0]);
+    });
+    document.getElementById("hungary").addEventListener("click", function () {
+        clickCountry(data.hungary[0]);
+    });
+    document.getElementById("england").addEventListener("click", function () {
+        clickCountry(data.england[0]);
+    });
+    document.getElementById("italy").addEventListener("click", function () {
+        clickCountry(data.italy[0]);
+    });
+    document.getElementById("cccp").addEventListener("click", function () {
+        clickCountry(data.cccp[0]);
     });
 }).catch(function (error) {
     return console.error(error);
 });
 
-//let jsonIteration = (arrayJson, data) =>{
-//   Object.keys(data).forEach(element => arrayJson.push(element));  
-//}
-
-//let clickCountry = (countryArray, countryArrayJson) =>{
-// for(let i=0; i<countryArray.length; i++){
-//     document.getElementById(countryArray[i]).addEventListener("click", ()=>{
-//         showCountry(countryArrayJson[i][0]);
-//     });            
-// }     
-//}
-
-var showCountry = function showCountry(country) {
-    document.getElementById("mainContainer").innerHTML = "";
-    document.getElementById("mainContainer").innerHTML = "country.title,  country.subtitle, country.position, country.area, country.adminDivision, country.capitol, country.biggestCities, country.population, country.naturalConditions, country.language, country.currency, country.historyPoliticalSystem, country.economy, country.map";
-};
+fetch(path + 'northAmerica.json').then(function (response) {
+    return response.json();
+}).then(function (data) {
+    document.getElementById("northAmericaDescription").addEventListener("click", function () {
+        clickCountry(data.northAmericaDescription[0]);
+    });
+    document.getElementById("bermudas").addEventListener("click", function () {
+        clickCountry(data.bermudas[0]);
+    });
+    document.getElementById("greenland").addEventListener("click", function () {
+        clickCountry(data.greenland[0]);
+    });
+    document.getElementById("canada").addEventListener("click", function () {
+        clickCountry(data.canada[0]);
+    });
+    document.getElementById("saintPierreMiquelon").addEventListener("click", function () {
+        clickCountry(data.saintPierreMiquelon[0]);
+    });
+    document.getElementById("usa").addEventListener("click", function () {
+        clickCountry(data.usa[0]);
+    });
+}).catch(function (error) {
+    return console.error(error);
+});
 
 /***/ })
 /******/ ]);
--- js/workingFile.js
+++ js/workingFile.js
@@ -1,51 +1,61 @@
 const path = 'https://szymekcendecki.github.io/atlas-85/json/';
 
-const europeArrayJson = [];
-
-const europeArray = ["europeDescription", "albania", "andorra", "austria", "belgium", "westernBerlin", "bulgaria", "czechoslovakia", "denmark", "finland", "france", "gibraltar", "greece", "spain", "holland", "irleand", "iceland", "yugoslavia", "liechtenstein", "luxembourg", "malta", "monaco", "eastGermany", "norway", "poland", "portugal", "westGermany", "romania", "sanMarino", "switzerland", "sweden", "vatican", "hungary", "england", "italy", "cccp"];
+let clickCountry = (country) =>{
+    document.getElementById("mainContainer").innerHTML = "";
+    document.getElementById("mainContainer").innerHTML = country.title + country.subtitle + country.position + country.area + country.adminDivision + country.capitol + country.biggestCities + country.population + country.naturalConditions + country.language + country.currency + country.historyPoliticalSystem + country.economy + country.map; 
+}
 
 fetch(path + 'europa.json')
 .then(response => response.json())
-.then(data => {
-   // Object.keys(data).forEach(element => console.log(element, element.length));  
-   //jsonIteration(europeArrayJson, data);
-    //clickCountry(europeArray, europeArrayJson);
-
-    document.getElementById("europeDescription").addEventListener("click", ()=>{
-        document.getElementById("mainContainer").innerHTML = "";
-        document.getElementById("mainContainer").innerHTML = data.europeDescription[0].title +
-        data.europeDescription[0].subtitle + data.europeDescription[0].position + data.europeDescription[0].area + data.europeDescription[0].adminDivision + data.europeDescription[0].capitol + data.europeDescription[0].biggestCities + data.europeDescription[0].population + data.europeDescription[0].naturalConditions + data.europeDescription[0].language + data.europeDescription[0].currency + data.europeDescription[0].historyPoliticalSystem + data.europeDescription[0].economy + data.europeDescription[0].map;
-   });     
-
-    document.getElementById("albania").addEventListener("click", ()=>{
-        document.getElementById("mainContainer").innerHTML = "";
-        document.getElementById("mainContainer").innerHTML = data.albania[0].title +
-        data.albania[0].subtitle + data.albania[0].position + data.albania[0].area + data.albania[0].adminDivision + data.albania[0].capitol + data.albania[0].biggestCities + data.albania[0].population + data.albania[0].naturalConditions + data.albania[0].language + data.albania[0].currency + data.albania[0].historyPoliticalSystem + data.albania[0].economy + data.albania[0].map;
-   });     
-   
-   document.getElementById("andorra").addEventListener("click", ()=>{
-    document.getElementById("mainContainer").innerHTML = "";
-    document.getElementById("mainContainer").innerHTML = data.andorra[0].title +
-    data.andorra[0].subtitle + data.andorra[0].position + data.andorra[0].area + data.andorra[0].adminDivision + data.andorra[0].capitol + data.andorra[0].biggestCities + data.andorra[0].population + data.andorra[0].naturalConditions + data.andorra[0].language + data.andorra[0].currency + data.andorra[0].historyPoliticalSystem + data.andorra[0].economy + data.andorra[0].map;
-});  
-   
-
+.then(data => {         
+    document.getElementById("europeDescription").addEventListener("click", ()=>{ clickCountry(data.europeDescription[0]); });     
+    document.getElementById("albania").addEventListener("click", ()=>{ clickCountry(data.albania[0]); });        
+    document.getElementById("andorra").addEventListener("click", ()=>{ clickCountry(data.andorra[0]); });     
+    document.getElementById("austria").addEventListener("click", ()=>{ clickCountry(data.austria[0]); });   
+    document.getElementById("belgium").addEventListener("click", ()=>{ clickCountry(data.belgium[0]); }); 
+    document.getElementById("westernBerlin").addEventListener("click", ()=>{ clickCountry(data.westernBerlin[0]); }); 
+    document.getElementById("bulgaria").addEventListener("click", ()=>{ clickCountry(data.bulgaria[0]); }); 
+    document.getElementById("czechoslovakia").addEventListener("click", ()=>{ clickCountry(data.czechoslovakia[0]); });
+    document.getElementById("denmark").addEventListener("click", ()=>{ clickCountry(data.denmark[0]); });
+    document.getElementById("finland").addEventListener("click", ()=>{ clickCountry(data.finland[0]); }); 
+    document.getElementById("france").addEventListener("click", ()=>{ clickCountry(data.france[0]); }); 
+    document.getElementById("gibraltar").addEventListener("click", ()=>{ clickCountry(data.gibraltar[0]); }); 
+    document.getElementById("greece").addEventListener("click", ()=>{ clickCountry(data.greece[0]); }); 
+    document.getElementById("spain").addEventListener("click", ()=>{ clickCountry(data.spain[0]); }); 
+    document.getElementById("holland").addEventListener("click", ()=>{ clickCountry(data.holland[0]); });  
+    document.getElementById("irleand").addEventListener("click", ()=>{ clickCountry(data.irleand[0]); }); 
+    document.getElementById("iceland").addEventListener("click", ()=>{ clickCountry(data.iceland[0]); }); 
+    document.getElementById("yugoslavia").addEventListener("click", ()=>{ clickCountry(data.yugoslavia[0]); });
+    document.getElementById("liechtenstein").addEventListener("click", ()=>{ clickCountry(data.liechtenstein[0]); }); 
+    document.getElementById("luxembourg").addEventListener("click", ()=>{ clickCountry(data.luxembourg[0]); });
+    document.getElementById("malta").addEventListener("click", ()=>{ clickCountry(data.malta[0]); });  
+    document.getElementById("monaco").addEventListener("click", ()=>{ clickCountry(data.monaco[0]); }); 
+    document.getElementById("eastGermany").addEventListener("click", ()=>{ clickCountry(data.eastGermany[0]); });
+    document.getElementById("norway").addEventListener("click", ()=>{ clickCountry(data.norway[0]); });  
+    document.getElementById("poland").addEventListener("click", ()=>{ clickCountry(data.poland[0]); });
+    document.getElementById("portugal").addEventListener("click", ()=>{ clickCountry(data.portugal[0]); });  
+    document.getElementById("westGermany").addEventListener("click", ()=>{ clickCountry(data.westGermany[0]); });
+    document.getElementById("romania").addEventListener("click", ()=>{ clickCountry(data.romania[0]); }); 
+    document.getElementById("sanMarino").addEventListener("click", ()=>{ clickCountry(data.sanMarino[0]); }); 
+    document.getElementById("switzerland").addEventListener("click", ()=>{ clickCountry(data.switzerland[0]); });
+    document.getElementById("sweden").addEventListener("click", ()=>{ clickCountry(data.sweden[0]); });
+    document.getElementById("vatican").addEventListener("click", ()=>{ clickCountry(data.vatican[0]); }); 
+    document.getElementById("hungary").addEventListener("click", ()=>{ clickCountry(data.hungary[0]); });
+    document.getElementById("england").addEventListener("click", ()=>{ clickCountry(data.england[0]); });
+    document.getElementById("italy").addEventListener("click", ()=>{ clickCountry(data.italy[0]); });
+    document.getElementById("cccp").addEventListener("click", ()=>{ clickCountry(data.cccp[0]); });    
 })
 .catch(error => console.error(error))
 
-//let jsonIteration = (arrayJson, data) =>{
- //   Object.keys(data).forEach(element => arrayJson.push(element));  
-//}
 
-//let clickCountry = (countryArray, countryArrayJson) =>{
-   // for(let i=0; i<countryArray.length; i++){
-   //     document.getElementById(countryArray[i]).addEventListener("click", ()=>{
-   //         showCountry(countryArrayJson[i][0]);
-   //     });            
-   // }     
-//}
-
-const showCountry = country =>{
-    document.getElementById("mainContainer").innerHTML = "";
-    document.getElementById("mainContainer").innerHTML = `country.title,  country.subtitle, country.position, country.area, country.adminDivision, country.capitol, country.biggestCities, country.population, country.naturalConditions, country.language, country.currency, country.historyPoliticalSystem, country.economy, country.map`;
-}
+fetch(path + 'northAmerica.json')
+.then(response => response.json())
+.then(data => {         
+    document.getElementById("northAmericaDescription").addEventListener("click", ()=>{ clickCountry(data.northAmericaDescription[0]); }); 
+    document.getElementById("bermudas").addEventListener("click", ()=>{ clickCountry(data.bermudas[0]); });
+    document.getElementById("greenland").addEventListener("click", ()=>{ clickCountry(data.greenland[0]); }); 
+    document.getElementById("canada").addEventListener("click", ()=>{ clickCountry(data.canada[0]); });
+    document.getElementById("saintPierreMiquelon").addEventListener("click", ()=>{ clickCountry(data.saintPierreMiquelon[0]); });
+    document.getElementById("usa").addEventListener("click", ()=>{ clickCountry(data.usa[0]); });          
+})
+.catch(error => console.error(error))
